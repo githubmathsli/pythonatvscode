d3deae66aff9dcf674472c9bfbf519f165413c2f
githubmathsli <puremathsgp@gmail.com>
2020-02-25 10:57:27 +0800

    Selenium Demo on Web Screen Capture

---
+++ websnap.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def test_fullpage_screenshot():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--start-maximized")
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver.get("https://www.w3schools.com")
+    time.sleep(2)
+
+    # the element with longest height on page
+    ele = driver.find_element("xpath", '//div[@class="react-grid-layout layout"]')
+    total_height = ele.size["height"] + 1000
+
+    driver.set_window_size(1920, total_height)  # the trick
+    time.sleep(2)
+    driver.save_screenshot("screenshot1.png")
+    driver.quit()
+
+
+if __name__ == "__main__":
+    test_fullpage_screenshot()
---
+++ websnap2.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from PIL import Image
+from selenium.webdriver.chrome.options import Options
+import time
+
+url = "https://www.w3schools.com"
+
+# run first time to get scrollHeight
+driver = webdriver.Chrome()
+driver.get(url)
+# pause 3 second to let page load
+time.sleep(3)
+# get scroll Height
+height = driver.execute_script(
+    "return Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight )"
+)
+print(height)
+# close browser
+driver.close()
+
+# Open another headless browser with height extracted above
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument(f"--window-size=1920,{height}")
+chrome_options.add_argument("--hide-scrollbars")
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get(url)
+# pause 3 second to let page loads
+time.sleep(3)
+# save screenshot
+driver.save_screenshot("screen_shot.png")
+driver.close()
